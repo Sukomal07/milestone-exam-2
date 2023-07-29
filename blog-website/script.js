a412f43612a45137b1c8bff736bc940f1d8ca1b5
Sukomal07 <hypersukomal@gmail.com>
2023-07-29 12:22:21 +0530

    complete project

--- blog-website/script.js
+++ blog-website/script.js
@@ -29,7 +29,6 @@ function getBlogsFromLocalStorage() {
 function displayBlogs() {
     const blogsData = getBlogsFromLocalStorage()
     const blogList = document.getElementById('blog-list')
-    blogList.innerHTML = ""
 
     blogsData.forEach((blog) => {
         const blogElement = createBlogElement(blog)
@@ -37,33 +36,49 @@ function displayBlogs() {
     });
 }
 
+function createBlogElement(blog) {
+    const newBlog = document.createElement("div")
+    newBlog.classList.add("blogs")
+    newBlog.innerHTML = `
+    <img src="${blog.imgUrl}" alt="Blog Image" class="blog-img">
+    <h3 class="blog-title">${blog.title}</h3>
+    <span class="blog-dec">${blog.description}</span>
+    <button class="read-blog">Read</button>
+    `
+
+    newBlog.querySelector('.read-blog').addEventListener("click", () => {
+        sessionStorage.setItem("currentBlog", JSON.stringify(blog))
+        window.location.href = './blog.html'
+    })
+    return newBlog
+}
+
 function addBlog(e) {
     e.preventDefault()
 
     const imgUrl = document.getElementById('img-url').value
-    const blogTitle = document.getElementById('blog-title').value
-    const blogDec = document.getElementById('blog-dec').value
-    const blogContent = document.getElementById('content').value
+    const title = document.getElementById('blog-title').value
+    const description = document.getElementById('blog-dec').value
+    const content = document.getElementById('content').value
 
-    const newBlog = document.createElement("div")
-    newBlog.classList.add("blogs")
-    newBlog.innerHTML = `
-    <img src="${imgUrl}" alt="Blog Image" class="blog-img">
-    <h3 class="blog-title">${blogTitle}</h3>
-    <span class="blog-dec">${blogDec}</span>
-    <button class="read-blog">Read</button>
-    `
-    newBlog.querySelector('.read-blog').addEventListener('click', () => {
-        sessionStorage.setItem("currentBlog", newBlog.innerHTML)
-        window.location.href = './blog.html'
-    })
 
-    const blogList = document.getElementById('blog-list')
-    blogList.appendChild(newBlog)
+    const newBlog = {
+        imgUrl,
+        title,
+        description,
+        content
+    }
+
+    const blogsData = getBlogsFromLocalStorage()
+    blogsData.push(newBlog)
+    saveBlogsToLocalStorage(blogsData)
+    displayBlogs()
     closeBlogModel()
 }
-
 
 document.getElementById('addNewBlog').addEventListener('click', openAddBlogModel)
 document.getElementById('close').addEventListener('click', closeBlogModel)
 document.getElementById('addBlog').addEventListener('click', addBlog)
+window.addEventListener('load', () => {
+    displayBlogs();
+});
