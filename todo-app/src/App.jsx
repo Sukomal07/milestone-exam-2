a6207f109576408a6b624b65cb066bd875714330
Sukomal07 <hypersukomal@gmail.com>
2023-08-01 13:00:02 +0530

    complete project

--- todo-app/src/App.jsx
+++ todo-app/src/App.jsx
@@ -1,26 +1,71 @@
+import { useState, useEffect } from 'react'
 import './App.css'
 
+const getTodoData = () => {
+  const storedTodos = JSON.parse(localStorage.getItem('todos')) || []
+  if (storedTodos) {
+    return storedTodos
+  }
+}
 function App() {
+  const [inputData, setInputData] = useState('')
+  const [todos, setTodos] = useState(getTodoData())
+  const addTodos = () => {
+    if (inputData !== '') {
+      setTodos([...todos, { title: inputData, status: 'Pending' }])
+      setInputData('')
+    }
+  }
+  const handleUpdateStatus = (index) => {
+    setTodos((prevTodos) => {
+      const updatedTodos = [...prevTodos];
+      const todo = { ...updatedTodos[index] };
+      todo.status = todo.status === "Pending" ? "Complete" : "Pending";
+      updatedTodos[index] = todo;
+      return updatedTodos;
+    });
+  };
+
+
+  const handleDelete = (index) => {
+    const updatedTodos = todos.filter((el, idx) => {
+      return idx !== index
+    })
+    setTodos(updatedTodos)
+  }
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   return (
     <>
       <div className="container">
         <div className="input-box">
-          <input type="text" placeholder='Create a todo' id='input' />
-          <button className='add'>Add</button>
+          <input type="text" placeholder='Create a todo' id='input' value={inputData} onChange={(e) => setInputData(e.target.value)} />
+          <button className='add' onClick={addTodos}>Add</button>
         </div>
         <div className="todo-container">
-          <div className="todos">
-            <div className="heading">
-              <span className="todo-number">1.</span>
-              <h1 className="todo-title">Create Project</h1>
-            </div>
-            <span className="todo-status">Status:Pending</span>
-            <div className="todo-btn">
-              <button className="update-status">Update Status</button>
-              <button className="remove">Remove</button>
-            </div>
-          </div>
+          {
+            todos.map((todo, idx) => {
+              return (
+                todo && (
+                  <div className="todos" key={idx}>
+                    <div className="heading">
+                      <span className="todo-number">{idx}.</span>
+                      <h1 className="todo-title">{todo.title}</h1>
+                    </div>
+                    <span className="todo-status">Status : {todo.status}</span>
+                    <div className="todo-btn">
+                      <button className="update-status" onClick={() => handleUpdateStatus(idx)}>Update Status</button>
+                      <button className="remove" onClick={() => handleDelete(idx)}>Remove</button>
+                    </div>
+                  </div>
+                )
+              );
+            })
+          }
+
         </div>
       </div>
     </>
